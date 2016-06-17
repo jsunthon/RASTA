50036efa2a0d3dfef2d9e593b74470a417b2fb6c
James Sunthonlap <jsunthon@gmail.com>
2016-06-17 11:25:15 -0700

    fix conflict

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -2,25 +2,24 @@
 var visualServApp = angular.module('visualServApp', ['ngRoute',
   'charts', 'upload', 'signup', 'login']);
 visualServApp.config(['$routeProvider', function ($routeProvider) {
-  // console.log("....sadsad");
-  // $routeProvider
-  //   .when('/home', {
-  //     templateUrl: '../../views/partials/charts.html',
-  //     controller: 'chartCtrl'
-  //   })
-  //   .when('/upload', {
-  //     templateUrl: '../../views/partials/upload.html',
-  //     controller: 'uploadCtrl'
-  //   })
-  //   .when('/signup', {
-  //     templateUrl: '../../views/partials/signup.html',
-  //     controller: 'signupCtrl'
-  //   })
-  //   .when('/login', {
-  //     templateUrl: '../../views/partials/login.html',
-  //     controller: 'loginCtrl'
-  //   })
-  //   .otherwise({
-  //     redirectTo: '/home'
-  //   });
+  $routeProvider
+    .when('/home', {
+      templateUrl: '../views/partials/charts.html',
+      controller: 'chartCtrl'
+    })
+    .when('/upload', {
+      templateUrl: '../views/partials/upload.html',
+      controller: 'uploadCtrl'
+    })
+    .when('/signup', {
+      templateUrl: '../views/partials/signup.html',
+      controller: 'signupCtrl'
+    })
+    .when('/login', {
+      templateUrl: '../views/partials/login.html',
+      controller: 'loginCtrl'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
 }]);
