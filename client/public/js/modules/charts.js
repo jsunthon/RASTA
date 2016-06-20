e77f4f334aafcea47a31de171eaac9c3ebb12432
James Sunthonlap <jsunthon@gmail.com>
2016-06-20 14:13:57 -0700

    wired data from api calls with ui

--- client/public/js/modules/charts.js
+++ client/public/js/modules/charts.js
@@ -11,38 +11,12 @@ charts.config(['ChartJsProvider', function (ChartJsProvider) {
 }]);
 
 charts.controller('chartCtrl', function ($scope, $timeout, $http) {
+  $scope.funcSelected = false;
+  $scope.funcServ = false;
 
-  //data for the overall service status
-  $scope.overallServStatLabels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.overallServStatSeries = ['All Services'];
-  $scope.overallServStatData = [
-    [65, 59, 80, 81, 56, 55, 40]
-  ];
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
-
-  //data for service availability
-  $scope.servAvailStatLabels = ["Up Time", "Down Time"];
-  $scope.servAvailStatData = [30, 70];
-
-
-  //array that holds the options for the "status by calls" chart
-  $scope.featureCalls = [{
-    "id": 1,
-    "name": "Sun Angle Data Fetcher",
-    "url": "http://pub.lmmp.nasa.gov:8083/getAzElfromT1/"
-  }, {
-    "id": 2,
-    "name": "Crater Detection Data Fetcher",
-    "url": "http://pub.lmmp.nasa.gov:8083/crossdomain.xml"
-  }, {
-    "id": 3,
-    "name": "Zoom Data Consumption",
-    "url": "http://50.18.111.140/crossdomain.xml"
-  }
-  ];
-
 
   //Current Date
   Date.prototype.today = function () {
@@ -54,77 +28,61 @@ charts.controller('chartCtrl', function ($scope, $timeout, $http) {
     return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
   }
 
-  //array that holds the options for the "status by features" chart
-  $scope.lmmpFeatures = [{
-    "id": 1,
-    "name": "Sun Angle",
-    "labels": ["January", "February", "March", "April", "May", "June", "July"],
-    "data": [65, 59, 80, 81, 56, 55, 40]
-  }, {
-    "id": 2,
-    "name": "Crater Check",
-    "labels": ["January", "February", "March", "April", "May", "June", "July"],
-    "data": [1, 2, 5, 100, 20, 55, 40]
-  }, {
-    "id": 3,
-    "name": "Rock Detector",
-    "labels": ["January", "February", "March", "April", "May", "June", "July"],
-    "data": [30, 10, 80, 10, 37, 55, 90]
-  }
-  ];
-
-  // //data for Function status
-  // $scope.funcStatLabels = ["January", "February", "March", "April", "May", "June", "July"];
-  // $scope.funcStatSeries = ['Sun Angle Calculation'];
-  // $scope.funcStatData = [
-  //   [65, 59, 80, 81, 56, 55, 40]
-  // ];
-
   var newDate = new Date();
   $scope.currTime = "Last Updated: " + newDate.today() + " @ " + newDate.timeNow();
 
-  //get data for overall serv availability
-
-
   //get data for overall serv stats
+  $http.get("/api/get_service_status").success(function (response) {
+    $timeout(function () {
+      $scope.overallServStatLabels = response.labels;
+      $scope.overallServStatData = [response.data];
+      $scope.overallServStatSeries = ["Overall Service Status"];
+
+      var availData = $scope.overallServStatData[0];
+      var avail = availData[availData.length - 1] * 100;
+      var unavail = 100 - avail;
+      $scope.servAvailStatData = [avail, unavail];
+      $scope.servAvailStatLabels = ["Available", "Unavailable"];
+    }, 100);
+  });
 
   //get data for the function status
   $http.get("/api/get_function_status").success(function (response) {
     $scope.funcArr = response.functions; //an array of function objects
   });
 
-  $scope.funcSelected = false;
+  //handles the event that a function is selected
   $scope.updateFeatData = function (featureSelected) {
     $timeout(function () {
+      $scope.funcServSelected = false;
       $scope.funcStatData = [
-        featureSelected.data
+        featureSelected.status.data
       ];
-      $scope.funcStatSeries = featureSelected.name;
-      $scope.funcStatLabels = featureSelected.labels;
+      $scope.funcStatSeries = [featureSelected.name];
+      $scope.funcStatLabels = featureSelected.status.labels;
       $scope.funcSelected = true;
+
+      getFunctionServices($scope.funcStatSeries);
     }, 0);
   }
 
+  function getFunctionServices(funcName) {
+    var baseUrl = "/api/get_service_status_by_function";
+    $http.get(baseUrl + "/function_name=" + funcName).success(function (response) {
+      $scope.funcServices = response.services;
+    });
+  }
 
-
-  //data for func service status
-  $scope.funcServStatLabels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.funcServStatSeries = ['Get Sun Data'];
-  $scope.funcServStatData = [
-    [65, 59, 80, 81, 56, 55, 40]
-  ];
-
-  // $scope.servSelected = false;
-  // $scope.updateServData = function (servSelected) {
-  //   $timeout(function () {
-  //     $scope.funcServStatLabels = [
-  //       servSelected.
-  //     ];
-  //     $scope.funcStatSeries = featureSelected.name;
-  //     $scope.funcStatLabels = featureSelected.labels;
-  //     $scope.funcSelected = true;
-  //   }, 0);
-  // }
+  $scope.updateFuncServData = function (servSelected) {
+    $timeout(function () {
+      $scope.funcServSelected = true;
+      $scope.funcServStatData = [
+        servSelected.status.data
+      ];
+      $scope.funcServStatSeries = [servSelected.name];
+      $scope.funcServStatLabels = servSelected.status.labels;
+    }, 0);
+  }
 });
 
 
