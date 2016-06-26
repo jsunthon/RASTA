a9221dd9e2fd73b8e967104a6e60693962a06d0c
jsunthon <jsunthon@gmail.com>
2016-06-25 17:47:40 -0700

    dropdown for small screens;consistent tab color layout;added wiki page;deleted signup page;success/error msgs after upload attempt;change navbar item font color

--- client/public/js/modules/upload.js
+++ client/public/js/modules/upload.js
@@ -1,37 +1,55 @@
 var upload = angular.module('upload', []);
 
 upload.directive('fileModel', ['$parse', function ($parse) {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      var model = $parse(attrs.fileModel);
-      var modelSetter = model.assign;
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
 
-      element.bind('change', function () {
-        scope.$apply(function () {
-          modelSetter(scope, element[0].files[0]);
-        });
-      });
-    }
-  };
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
 }]);
 
+upload.controller('uploadCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+    $scope.uploadFile = function () {
+        if ($scope.jsonUploaded) {
+            $scope.jsonUploaded = false;
+        }
+        var file = $scope.myFile;
+        $scope.uploadAttempt = true;
+        $scope.statusStyle = {
+            "font-style": "italic",
+            "font-weight": "bold"
+        };
+        if (file) {
+            var uploadUrl = "/api/post_api_list";
+            uploadFileToUrl(file, uploadUrl);
+        } else {
+            $scope.statusColor = "text-danger";
+            $scope.uploadStatus = "No JSON file was selected.";
+        }
+        $timeout(function () {
+            $scope.uploadAttempt = false;
+        }, 3000);
+    };
 
-upload.controller('uploadCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-  $scope.uploadFile = function () {
-    var file = $scope.myFile;
-    var uploadUrl = "/api/post_api_list";
-    uploadFileToUrl(file, uploadUrl);
-  };
-
-  var uploadFileToUrl = function (file, uploadUrl) {
-    $http.post(uploadUrl, file, {headers: {'Content-Type': 'application/json'}})
-      .success(function (res) {
-        $scope.jsonRsp = JSON.stringify(res, null, 2);
-        $scope.jsonUploaded = true;
-      })
-      .error(function () {
-        $scope.jsonRsp = "upload unsuccessful.";
-      });
-  }
+    var uploadFileToUrl = function (file, uploadUrl) {
+        $http.post(uploadUrl, file, {headers: {'Content-Type': 'application/json'}})
+            .success(function (res) {
+                $scope.jsonRsp = JSON.stringify(res, null, 2);
+                $scope.jsonUploaded = true;
+                $scope.statusColor = "text-success";
+                $scope.uploadStatus = "Upload successfully processed.";
+            })
+            .error(function () {
+                $scope.statusColor = "text-danger";
+                $scope.uploadStatus = "Error in processing upload.";
+            });
+    }
 }]);
