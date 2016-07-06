bca93131e98073a6612cf69fcd09529b2f6162d9
Ray Y Han <giraiday@gmail.com>
2016-07-06 15:47:43 -0700

    ticket backend

--- db/models/issue_ticket.js
+++ db/models/issue_ticket.js
@@ -20,22 +20,25 @@ ticket_schema.pre('save', function (next) {
   this.open_day = this.open_date.getDate();
   this.open_month = this.open_date.getMonth() + 1;
   this.open_year = this.open_date.getFullYear();
-  console.log(this.issues);
-  mongoose.model('TestResult').find({_id: this.issues[0]}, function (err, found) {
-    console.log(found);
-  });
+
+  var self = this;
+
+  //console.log(this.issues);
   mongoose.model('TestResult').find({ _id: { $in: this.issues } }, function (err, found_results) {
     if (err) return console.error(err);
     //console.log(found_results);
-    this.bad_services = found_results.map(function (result) {
+    self.bad_services = found_results.map(function (result) {
       return result.service_id;
     });
+    self.bad_services.sort();
+    //console.log(self.bad_services);
+    //console.log(self.open_day);
     Ticket.find(
       {
-        open_day: this.open_day,
-        open_month: this.open_month,
-        open_year: this.open_year,
-        bad_services: this.bad_services
+        open_day: self.open_day,
+        open_month: self.open_month,
+        open_year: self.open_year,
+        bad_services: self.bad_services
       },
       function (err, found_tickets) {
         console.log(found_tickets);
