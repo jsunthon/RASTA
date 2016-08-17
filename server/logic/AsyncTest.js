8cd3d70f3a08beb040191f8fbcf4b288989881d5
James Sunthonlap <jsunthon@gmail.com>
2016-08-17 12:02:27 -0700

    fix some bug in AsyncTest.js

--- server/logic/AsyncTest.js
+++ server/logic/AsyncTest.js
@@ -5,22 +5,25 @@ var parser = require('xml2js').parseString;
 
 function AsyncTest() {
   var self = this;
+
   this.dbManager = new AsyncCallDbManager();
+
   this.submitJobs = function () {
-    this.dbManager.retrieveAsyncCall().then(function (call_objs) {
-      var urls = this.createAllUrls(call_objs);
+    self.dbManager.retrieveAsyncCall().then(function (call_objs) {
+      var urls = self.createAllUrls(call_objs);
+      console.log('urls: ss' + urls);
       self.createJobs(urls);
     });
   };
 
   this.testJobs = function () {
-    this.dbManager.retrieveAsyncCall().then(function (call_objs) {
+    self.dbManager.retrieveAsyncCall().then(function (call_objs) {
       var checker_urls = call_objs.map(function (call_obj) {
         return call_obj.job_checker;
       });
       checker_urls.reduce(function (pre, cur) {
         pre.then(function () {
-          return this.testAsynceProgress(cur);
+          return self.testAsynceProgress(cur);
         })
       }, Promise.resolve());
     });
@@ -28,16 +31,17 @@ function AsyncTest() {
 
   this.createAllUrls = function (call_objs) {
     var urls_list = call_objs.map(function (call_obj) {
-      self.createAUrlGroup(call_obj.job_creator.base_url, call_obj.job_creator.parameters);
+      return self.createAUrlGroup(call_obj.job_creator.base_url, call_obj.job_creator.parameters);
     });
+    console.log('url list: ' + urls_list);
     return urls_list.reduce(function (pre, cur) {
       return pre.concat(cur);
-    }, [])
+    }, []);
   };
 
   this.createAUrlGroup = function (base_url, params_list) {
     return params_list.map(function (params) {
-      self.createAUrl(base_url, params);
+      return self.createAUrl(base_url, params);
     });
   };
 
@@ -93,6 +97,7 @@ function AsyncTest() {
   };
 
   this.authorize = function () {
+    console.log('calling this.authorize');
     var login_url = 'https://ops.lmmp.nasa.gov/opensso/UI/Login';
     var credential_form = {form: {IDToken1: 'lmmpdev', IDToken2: 'devlmmp'}};
     return new Promise(function (resolve, reject) {
@@ -100,21 +105,27 @@ function AsyncTest() {
         if (err) {
           console.error(err);
           reject();
+        } else {
+          console.log('successfully authorized.');
+          resolve();
         }
-        resolve();
       })
     });
   };
 
   this.testAsynceProgress = function (url) {
+    console.log('url async prog: ' + url);
     return new Promise(function (resolve) {
       self.authorize().then(function () {
         request.get(url, function (err, res, body) {
+          console.log('res: ' + JSON.stringify(res));
+          console.log('body: ' + JSON.stringify(body));
           parser(body, function (err, result) {
             var keys = Object.keys(result.Result);
             var arr = result.Result[keys[0]];
+            console.log(keys);
             arr.map(function (em) {
-              this.checkOneResult(em, url, res)
+              self.checkOneResult(em, url, res)
             });
           });
         });
@@ -127,11 +138,13 @@ function AsyncTest() {
       var que_date = new Date(result.Status[0].Enquequed[0]);
       var cur_date = new Date().valueOf();
       var hours_elapse = (cur_date - que_date) / 1000 / 3600;
+      console.log('hours_elapse' + hours_elapse);
+      console.log(JSON.stringify(result));
       if (hours_elapse < 25 && hours_elapse > 23) {
         if (result.Status.Completed === undefined) {
-          this.dbManager.retrieveACall(url).then(function (found_call) {
+          self.dbManager.retrieveACall(url).then(function (found_call) {
             if (found_call) {
-              this.createATestResult(url, response, hours_elapse).then(resolve());
+              self.createATestResult(url, response, hours_elapse).then(resolve());
             }
           });
         }
@@ -142,4 +155,6 @@ function AsyncTest() {
 
 
 var tester = new AsyncTest();
-tester.testAsynceProgress('https://raw.githubusercontent.com/jsunthon/RASTA/master/sample_pages/result.xml?token=AJt5DRk2eiX8F5G6AYFQeW_11TVo09Apks5XvbiuwA%3D%3D');
+// tester.testAsynceProgress('https://raw.githubusercontent.com/jsunthon/RASTA/master/sample_pages/result.xml?token=AJt5DRk2eiX8F5G6AYFQeW_11TVo09Apks5XvbiuwA%3D%3D');
+// tester.submitJobs();
+tester.testJobs();
